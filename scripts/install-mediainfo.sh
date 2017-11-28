c5b8a4b32c07923921c95e503693d95bcf2a1c7f
David Sykora <dsykora@gmail.com>
2017-11-27 23:50:07 -0800

    possible fixes for media info install

--- scripts/install-mediainfo.sh
+++ scripts/install-mediainfo.sh
@@ -1,23 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-BUILD_DIR=$1
-CACHE_DIR=$2
-MEDIAINFO_VERSION="0.7.91"
-ARCHIVE="mediainfo-${MEDIAINFO_VERSION}.tar.gz"
+printf "\n[-] Installing MEDIA INFO...\n\n"
 
-# config
-echo "-----> Extract mediainfo"
-mkdir -p "${BUILD_DIR}/vendor"
-rm -rf "${BUILD_DIR}/vendor/mediainfo"
-tar -xf "${ARCHIVE}" -C "${BUILD_DIR}/vendor"
-chown -R node:node $BUILD_DIR
-echo "-----> chmod +x mediainfo"
-chmod +x "${BUILD_DIR}/vendor/mediainfo/mediainfo"
+############################################
+# MEDIA INFRO
+############################################
+
+apt-get update
+apt-get install -y wget chrpath libssl-dev libxft-dev
+
+cd /tmp
+wget https://s3.amazonaws.com/torsh-talent-build-assets/mediainfo-0.7.91.tar.gz
+tar -xzf mediainfo-0.7.91.tar.gz
+
+chown -R node:node mediainfo
+chmod +x mediainfo/mediainfo
+
+mv mediainfo /usr/local/share
+
+chown -R node:node /usr/local/share/mediainfo
+chmod +x /usr/local/share/mediainfo/mediainfo
+
+ln -sf /usr/local/share/mediainfo/mediainfo /usr/local/bin/mediainfo
+ln -sf /usr/local/share/mediainfo/mediainfo /usr/bin/mediainfo
 
 echo "-----> Exporting PATH and LIBRARY_PATH"
 PROFILE_PATH="$BUILD_DIR/.profile.d/mediainfo.sh"
 mkdir -p $(dirname $PROFILE_PATH)
-echo 'export PATH="$PATH:$HOME/vendor/mediainfo"' >> $PROFILE_PATH
+echo 'export PATH="$PATH:/usr/local/share/mediainfo"' >> $PROFILE_PATH
 echo "-----> MEDIAINFO " $MEDIAINFO_VERSION " COMPLETED"
+
+apt-get -y purge wget
