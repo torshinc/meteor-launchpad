e16e3846f6b5d74305c28789cb903a91e276878c
David Sykora <dsykora@gmail.com>
2017-11-28 23:12:05 -0800

    Test installing Oracle java 8

--- scripts/install-libreoffice.sh
+++ scripts/install-libreoffice.sh
@@ -8,8 +8,17 @@ set -e
 echo "-----> Install LibreOffice: Beginning Libre Office Extras installation."
 set -e
 
+
+# Test installing Oracle java 8
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
+add-apt-repository -y ppa:webupd8team/java
 apt-get update
-apt-get -y -q install default-jdk openjdk-7-jre libreoffice libreoffice-writer ure libreoffice-java-common libreoffice-base libreoffice-core libreoffice-common fonts-opensymbol hyphen-fr hyphen-de hyphen-en-us hyphen-it hyphen-ru fonts-dejavu fonts-dejavu-core fonts-dejavu-extra fonts-noto fonts-dustin fonts-f500 fonts-fanwood fonts-freefont-ttf fonts-liberation fonts-lmodern fonts-lyx fonts-sil-gentium fonts-texgyre fonts-tlwg-purisa
+apt-get install -y oracle-java8-installer
+rm -rf /var/lib/apt/lists/*
+rm -rf /var/cache/oracle-jdk8-installer
+
+apt-get update
+apt-get -y -q install default-jdk libreoffice libreoffice-writer ure libreoffice-java-common libreoffice-base libreoffice-core libreoffice-common fonts-opensymbol hyphen-fr hyphen-de hyphen-en-us hyphen-it hyphen-ru fonts-dejavu fonts-dejavu-core fonts-dejavu-extra fonts-noto fonts-dustin fonts-f500 fonts-fanwood fonts-freefont-ttf fonts-liberation fonts-lmodern fonts-lyx fonts-sil-gentium fonts-texgyre fonts-tlwg-purisa
 apt-get -q -y remove libreoffice-gnome
 
 #BUILD_DIR=$1
