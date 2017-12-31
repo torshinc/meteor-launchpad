52af2d4c62a9e55c10dd72b1975af49ba567c3d5
David Sykora <dsykora@gmail.com>
2017-12-30 18:49:33 -0800

    make ffprobe available to command line

--- scripts/install-ffmpeg.sh
+++ scripts/install-ffmpeg.sh
@@ -17,14 +17,20 @@ tar -xzf ffmpeg3.tar.gz
 
 chown -R node:node ffmpeg
 chmod +x ffmpeg/bin/ffmpeg
+chmod +x ffmpeg/bin/ffprobe
 
 mv ffmpeg /usr/local/share
 
 chown -R node:node /usr/local/share/ffmpeg
 chmod +x /usr/local/share/ffmpeg/bin/ffmpeg
+chmod +x /usr/local/share/ffmpeg/bin/ffprobe
 
 ln -sf /usr/local/share/ffmpeg/bin/ffmpeg /usr/local/share/ffmpeg
 ln -sf /usr/local/share/ffmpeg/bin/ffmpeg /usr/local/bin/ffmpeg
 ln -sf /usr/local/share/ffmpeg/bin/ffmpeg /usr/bin/ffmpeg
+
+ln -sf /usr/local/share/ffmpeg/bin/ffprobe /usr/local/share/ffprobe
+ln -sf /usr/local/share/ffmpeg/bin/ffprobe /usr/local/bin/ffprobe
+ln -sf /usr/local/share/ffmpeg/bin/ffprobe /usr/bin/ffprobe
 
 apt-get -y purge wget
