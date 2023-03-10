f3ac77cc17fdb34da6850a84ec835b2dbb50ad1a
Dsyko <dsykora@gmail.com>
2023-03-10 00:58:14 -0800

    Try to echo the version number

--- scripts/install-wkhtmltopdf.sh
+++ scripts/install-wkhtmltopdf.sh
@@ -67,20 +67,16 @@ echo "-----> Install wkhtmltopdffff"
 apt-get update
 apt-get install -y wget chrpath libssl-dev libxft-dev
 
-# from https://stackoverflow.com/questions/70073176/docker-install-wkhtmltopdf-error-updating-from-0-12-4-to-0-12-6
-apt install -y fontconfig fontconfig-config fonts-dejavu-core libbsd0 \
-libfontconfig1 libfontenc1 libfreetype6 libmd0 libpng16-16 \
-libx11-6 libx11-data libxau6 libxcb1 libxdmcp6 libxext6 libxrender1 sensible-utils \
-ucf x11-common xfonts-75dpi xfonts-base xfonts-encodings xfonts-utils
-
 # Download and install wkhtmltopdf
 cd /tmp
 wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb
 dpkg -i wkhtmltox_0.12.6-1.bionic_amd64.deb
 
-chown -R node:node /usr/local/bin/wkhtmltopdf
-chmod +x /usr/local/bin/wkhtmltopdf
-ln -sf /usr/local/bin/wkhtmltopdf /usr/bin/wkhtmltopdf
+# chown -R node:node /usr/local/bin/wkhtmltopdf
+# chmod +x /usr/local/bin/wkhtmltopdf
+# ln -sf /usr/local/bin/wkhtmltopdf /usr/bin/wkhtmltopdf
+
+echo "Current version of wkhtmltopdf: $(wkhtmltopdf --version)"
 
 # Cleanup
 rm -f wkhtmltox_0.12.6-1.bionic_amd64.deb
